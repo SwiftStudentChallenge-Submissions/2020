665faa091b078d85cd6939d37b6e38e001cba872
Piotr Jeremicz <piotrek@jeremicz.com>
2025-02-05 13:35:24 +0100

    Removed old files

--- Submission/jankowalski.swift
+++
@@ -1,9 +0,0 @@
-Submission(
-    name: "Jan Kowalski",
-    status: .accepted,
-    technologies: ["SwiftUI"],
-    
-    aboutMeUrl: nil,
-    sourceUrl: nil,
-    videoUrl: nil
-)
--- Submission/piotrjeremicz.swift
+++
@@ -1,9 +0,0 @@
-Submission(
-    name: "Piotr Jeremicz",
-    status: .accepted,
-    technologies: ["UIKit", "AVKit", "AVFoundation"],
-    
-    aboutMeUrl: .init(string: "https://github.com/piotrekjeremicz"),
-    sourceUrl: nil,
-    videoUrl: .init(string: "https://youtu.be/H4da5dqhcxY")
-)
